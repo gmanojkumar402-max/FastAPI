73bff71f466140b071c6b3d4e6b7d4bbab2e339a
gmano <gmanojkumar402@gmail.com>
2025-12-24 08:45:55 -0800

    Add Time_lines_files

---
+++ Time_lines/sync_demo.py
@@ -0,0 +1,13 @@
+import time
+from timeit import default_timer as timer 
+
+def run_task(name, seconds):
+    print(f'{name} started at: {timer()}')
+    time.sleep(seconds)
+    print(f'{name} comp[leted at : {timer()}]')
+
+start = timer()
+run_task('Task 1',2)
+run_task('Task 2',1)
+run_task('Task 3',3)
+print(f'\n Total timetaken: {timer() - start: .2f} s')
